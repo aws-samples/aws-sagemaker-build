904496a5c01cce74bb0d4eb2799066e9de495f60
John M Calhoun <johnmcalhoun123@gmail.com>
2018-02-27 23:02:42 -0500

    fix typo in settime lambda"

--- cloudformation-template/step_function/lambdas/SetTime.js
+++ cloudformation-template/step_function/lambdas/SetTime.js
@@ -3,7 +3,7 @@ exports.handler=(event,context,cb)=>{
     console.log("EVENT:",JSON.stringify(event,null,2))
     var date=new Date()
 
-    event.timestamp=`${date.getFullYear().toString()}-${date.getMonth()+1}-${date.getDate()}-${date.getHours()}-${date.getMinutes()}-${date.getSeconds()}-${datae.getMilliseconds}`
+    event.timestamp=`${date.getFullYear().toString()}-${date.getMonth()+1}-${date.getDate()}-${date.getHours()}-${date.getMinutes()}-${date.getSeconds()}-${date.getMilliseconds}`
 
     event.name=`${event.StackName}-${event.timestamp}`
     cb(null,event)
