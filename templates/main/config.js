57a862e0016abf1dd9391da429da12cdfb59798b
Aaron Beckett <arockett@amazon.com>
2019-07-24 12:25:48 -0400

    Fix issue which ignored ConfigFramework setting
    Without this fix, the ConfigFramework set in the parameter store
    will always be set to BYOD after deploying the stack regardless
    of the value in the toplevel config file.

--- templates/main/config.js
+++ templates/main/config.js
@@ -2,8 +2,8 @@ var base=require('../../config')
 
 module.exports=Object.assign(base,{
     name:"main",
-    parameters:{
+    parameters:Object.assign(base.parameters,{
         "AssetBucket":base.templateBucket,
-        "AssetPrefix":base.templatePrefix,
-    }
+        "AssetPrefix":base.templatePrefix
+    })
 })
