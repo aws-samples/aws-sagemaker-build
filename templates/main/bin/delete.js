588549ad7a31d63d84ae704f7f430a3a3553d13b
Aaron Beckett <arockett@amazon.com>
2019-07-24 12:22:52 -0400

    Fix config path in delete script
    Without this change, the delete script will fail
    with an import error.

--- templates/main/bin/delete.js
+++ templates/main/bin/delete.js
@@ -1,11 +1,11 @@
 #! /usr/bin/env node
 var check=require('./check')
 var aws=require('aws-sdk')
-var config=require('../../config')
+var config=require('../config')
 var cf=new aws.CloudFormation({region:config.region})
 var name=require('./name')
 var wait=require('./wait')
-var bucket=require('../../config').templateBucket
+var bucket=require('../config').templateBucket
 if(require.main===module){
     run()
 }
